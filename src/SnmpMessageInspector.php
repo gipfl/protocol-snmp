c7efded9d511de0cb263683bba4270da29db5e08
Thomas Gelf <thomas@gelf.net>
2019-01-07 14:23:01 +0100

    SnmpMessageInspector.php: allow to get dump

--- src/SnmpMessageInspector.php
+++ src/SnmpMessageInspector.php
@@ -6,18 +6,25 @@ class SnmpMessageInspector
 {
     public static function dump(SnmpMessage $message)
     {
-        printf("Version: %s\n", $message->getVersion());
+        echo static::getDump($message);
+    }
+
+    public static function getDump(SnmpMessage $message)
+    {
+        $result = sprintf("Version: %s\n", $message->getVersion());
         if ($message instanceof SnmpV1Message) {
-            printf("Community: %s\n", $message->getCommunity());
+            $result .= sprintf("Community: %s\n", $message->getCommunity());
         }
 
         /** @var VarBind $varBind */
         foreach ($message->getPdu()->getVarBinds()->iterate() as $varBind) {
-            printf(
+            $result .= sprintf(
                 "%s: %s\n",
                 $varBind->getOid(),
                 $varBind->getValue()->getReadableValue()
             );
         }
+
+        return $result;
     }
 }
